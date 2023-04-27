bf321f2ab983ffe355cea6d02e81b1c1fc52290d
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2023-04-27 13:19:38 -0800

    feat: text boxes for user input (site)

--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -1,5 +1,49 @@
 <script>
+	import CryptoJS from 'crypto-js';
 
+	// HMAC w/ CryptoJS
+	const creation = (privateKey, message) => {
+		const hash = CryptoJS.HmacSHA256(message, privateKey);
+		return hash;
+	};
+
+	function add(a, b) {
+		return a + b;
+	}
+
+	function diagnostics() {
+		let flag = 'Hello, world.';
+		console.log(flag);
+	}
+
+	let width;
+	let height;
+	let size = 42;
+	let text = 'input';
+
+	import { onMount } from 'svelte';
+
+	let time = new Date();
+
+	$: hours = time.getHours();
+	$: minutes = time.getMinutes();
+	$: seconds = time.getSeconds();
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			time = new Date();
+		}, 1000);
+
+		return () => {
+			clearInterval(interval);
+		};
+	});
+
+	// [I/O]
+
+	let initial = 'abc';
+
+	let final = 'xyz';
 </script>
 
 <div class="index">
@@ -8,6 +52,67 @@
 	</div>
 
 	<div>
+		<h3>
+			<a href="https://en.wikipedia.org/wiki/HMAC"><em>HmacSHA256</em></a>
+		</h3>
+
+		<p>Hash-based Message Authentication Codes</p>
+
+		<div>
+			<input type="text" id="key" name="key" placeholder="Key" bind:value={initial} />
+
+			<br />
+
+			<input type="text" id="data" name="data" placeholder="Data" bind:value={final} />
+
+			<br />
+
+			<!-- Pass values from input box to created value. -->
+
+			<div>
+				<p>
+					Output:
+
+					{creation(initial, final)}
+				</p>
+			</div>
+		</div>
+
+		<hr />
+
+		<h3>Object Permutation</h3>
+
+		<h5>Permutation vs. Combination</h5>
+
+		<!-- 5 x 5 grid  -->
+
+		<input type="radio" />
+
+		<p>Clock</p>
+
+		<p>{time.toLocaleTimeString()}</p>
+
+		<button on:click={diagnostics}>Diagnostics</button>
+
+		<h3>Sponge Demostration</h3>
+
+		<input type="range" bind:value={size} />
+		<input bind:value={text} />
+
+		<p>size: {width}px x {height}px</p>
+
+		<div bind:clientWidth={width} bind:clientHeight={height}>
+			<span>{text}</span>
+		</div>
+	</div>
+
+	<br />
+
+	<div>
+		<h2>Ascon Implementation</h2>
+
+		<em>Coming soon!</em>
+
 		<h3>Encryption</h3>
 
 		<form method="POST">
@@ -18,19 +123,9 @@
 			<button>Send</button>
 		</form>
 
-		<!-- <button >Hello, world.</button> -->
-
-		<!-- <label for="plaintext">Plaintext</label> -->
-
-		<!-- <form>
-			<input type="text" id="plaintext" name="plaintext" value="Hello World!" />
-		</form> -->
-
 		<h3>Decryption</h3>
 
-		<!-- <form>
-			<input type="text" id="plaintext" name="plaintext" value="Hello World!" />
-		</form> -->
+		<h3>Applications</h3>
 	</div>
 
 	<div>
--- site/src/routes/about/+page.svelte
+++ site/src/routes/about/+page.svelte
@@ -1,9 +1,7 @@
 <div class="about">
 	<h2>About</h2>
 	<p>
-		Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus laudantium quidem exercitationem
-		cumque dicta quis non, ipsa obcaecati ipsam autem necessitatibus sequi incidunt asperiores, modi
-		earum, numquam ex reprehenderit minus.
+		[TODO: Applications of LWC]
 	</p>
 </div>
 
