242c89eb451c982258cf977c04ca94f99f5b118c
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2023-04-26 23:59:44 -0800

    feat: HMAC of key + data

--- lib/lwc.rb
+++ lib/lwc.rb
@@ -2,21 +2,41 @@
 # CS 665 (Final Project)
 # 2023-04-07
 #
-# Simple hashing function of a string.
+# Simple hashing function with demo string.
 
 require 'digest'
+require 'openssl'
 
 class LWC
   def initialize
     @hash = Digest::SHA256.new
   end
 
-  def hash_string(string)
-    @hash.hexdigest(string)
+  def hash_string(plaintext)
+    @hash.hexdigest(plaintext)
+  end
+
+  # def hash_file(filename)
+  #   File.open(filename, 'rb') do |file|
+  #     buffer = ''
+  #     @hash.update(buffer) until file.eof
+  #   end
+  #   @hash.hexdigest
+  # end
+
+  # Hash-based Message Authentication Code
+  # http://en.wikipedia.org/wiki/HMAC
+  def generation
+    key = 'key'
+    data = 'The quick brown fox jumped over the lazy dog.'
+    hmac = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), key, data)
+    puts hmac
   end
 end
 
-string = "Lightweight Cryptography"
+input = "Lightweight Cryptography"
 
 lwc = LWC.new
-puts lwc.hash_string(string)
+puts lwc.hash_string(input)
+# hash_file = lwc.hash_file('example.txt')
+lwc.generation
